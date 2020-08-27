05c033009360a957dd7a370f1d4a18033d1368b4
lee-yujinn <leeyj7988@naver.com>
2020-08-27 22:37:30 +0900

    [Feat] cell 뷰

---
+++ iOS_xib_pratice/iOS_xib_pratice/MyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTableViewCell.swift
+//  iOS_xib_pratice
+//
+//  Created by 이유진 on 2020/08/27.
+//  Copyright © 2020 이유진. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ iOS_xib_pratice/iOS_xib_pratice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_xib_pratice
+//
+//  Created by 이유진 on 2020/08/27.
+//  Copyright © 2020 이유진. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
